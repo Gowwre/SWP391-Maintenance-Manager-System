a0bede5e09fcc2406d200b590fbb237a13a70eba
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-02-07 17:05:38 +0700

    Update MaintenanceManager.java

--- MaintenanceManagerSystem/src/main/java/com/fpt/maintenancemanagersystem/model/MaintenanceManager.java
+++ MaintenanceManagerSystem/src/main/java/com/fpt/maintenancemanagersystem/model/MaintenanceManager.java
@@ -4,10 +4,60 @@
  */
 package com.fpt.maintenancemanagersystem.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Radriar
  */
-public class MaintenanceManager {
-    
+public class MaintenanceManager implements Serializable {
+    private int managerID;
+    private String name;
+    private String email;
+    private String password;
+
+    public MaintenanceManager() {
+    }
+
+    public int getManagerID() {
+        return managerID;
+    }
+
+    public void setManagerID(int managerID) {
+        this.managerID = managerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "MaintenanceManager{" +
+                "managerID=" + managerID +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
