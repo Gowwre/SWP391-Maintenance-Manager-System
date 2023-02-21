067be355da13ac462fa5bc2ef9fc97aa3cd437cd
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-02-21 23:10:19 +0700

    refactor: create 2 new controllers based on domain

--- maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/controller/ManagerController.java
+++ maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/controller/ManagerController.java
@@ -1,20 +1,17 @@
 package com.fptu.maintenancemanagersystem.controller;
 
 import com.fptu.maintenancemanagersystem.model.Manager;
-import com.fptu.maintenancemanagersystem.model.Staff;
 import com.fptu.maintenancemanagersystem.service.Manager.ManagerService;
-import com.fptu.maintenancemanagersystem.service.Staff.StaffService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class AuthController {
-    @Autowired
-    StaffService staffService;
-
+public class ManagerController {
     @Autowired
     ManagerService managerService;
     @GetMapping("/managerLogin")
@@ -22,28 +19,8 @@ public class AuthController {
         model.addAttribute("manager",new Manager());
         return "loginPages/maintenanceManagerLogin";
     }
-
-    @GetMapping("/staffLogin")
-    public String showStaffLoginForm(Model model){
-        model.addAttribute("staff",new Staff());
-        return "loginPages/maintenanceStaffLogin";
-    }
-
-   @PostMapping("/staffLogin")
-    public String staffLogin(@ModelAttribute Staff staff, Model model, HttpSession session){
-        Staff existedStaff = staffService.findUserByLogin(staff.getEmail(), staff.getPassword());
-        if(existedStaff!=null){
-            session.setAttribute("staff",existedStaff);
-            return "homePages/maintenanceStaffHomePage";
-        }
-        else{
-            String errorMessage = "Sai thông tin đăng nhập. Vui lòng thử lại.";
-            model.addAttribute("errorMessage", errorMessage);
-            return "loginPages/maintenanceStaffLogin";
-    }}
-
     @PostMapping(value = {"/managerLogin"})
-    public String managerLogin(@ModelAttribute("manager") Manager manager  ,HttpSession session, Model model){
+    public String managerLogin(@ModelAttribute("manager") Manager manager  , HttpSession session, Model model){
         Manager existedManager = managerService.findUserByLogin(manager.getEmail(), manager.getPassword());
         if(existedManager != null){
             session.setAttribute("manager", existedManager);
---
+++ maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/controller/StaffController.java
@@ -0,0 +1,44 @@
+package com.fptu.maintenancemanagersystem.controller;
+
+import com.fptu.maintenancemanagersystem.model.Staff;
+import com.fptu.maintenancemanagersystem.service.Staff.StaffService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StaffController {
+    @Autowired
+    StaffService staffService;
+
+
+    @GetMapping("/staffLogin")
+    public String showStaffLoginForm(Model model) {
+        model.addAttribute("staff", new Staff());
+        return "loginPages/maintenanceStaffLogin";
+    }
+
+    @PostMapping("/staffLogin")
+    public String staffLogin(@ModelAttribute Staff staff, Model model, HttpSession session) {
+        Staff existedStaff = staffService.findUserByLogin(staff.getEmail(), staff.getPassword());
+        if (existedStaff != null) {
+            session.setAttribute("staff", existedStaff);
+            return "homePages/maintenanceStaffHomePage";
+        } else {
+            String errorMessage = "Sai thông tin đăng nhập. Vui lòng thử lại.";
+            model.addAttribute("errorMessage", errorMessage);
+            return "loginPages/maintenanceStaffLogin";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/";
+    }
+
+}
