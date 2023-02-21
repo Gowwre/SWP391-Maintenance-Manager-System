d49f9f5dcb94097cfcfa8948757af6b92fe30497
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-02-21 21:02:43 +0700

    feat: remove direct access to homepages

--- MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/controller/MainController.java
+++ MaintenanceManagerSystem/src/main/java/com/fptu/maintenancemanagersystem/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.fptu.maintenancemanagersystem.controller;
 
-import com.fptu.maintenancemanagersystem.model.Person;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,32 +25,14 @@ public class MainController {
         return "reportForm/residentReportFaultDevice";
     }
 
-    @RequestMapping(value = {"/managerLogin"},method = RequestMethod.GET)
-    public String managerLogin(){
-        return "loginPages/maintenanceManagerLogin";
-    }
 
-    @RequestMapping(value = {"/staffLogin"},method = RequestMethod.GET)
-    public String staffLogin(){
-        return "loginPages/maintenanceStaffLogin";
-    }
 
     @RequestMapping(value = {"/forgotPassword"},method = RequestMethod.GET)
     public String forgotPassword(){
         return "passwordProblemPages/forgotPassword";
     }
 
-    //Gains temporary access to staff homepage
-    @RequestMapping(value = {"/staffHome"},method = RequestMethod.GET)
-    public String staffHome(){
-        return "homePages/maintenanceStaffHomePage";
-    }
 
-    //Gains temporary access to manager homepage
-    @RequestMapping(value = {"/managerHome"},method = RequestMethod.GET)
-    public String managerHome(){
-        return "homePages/managerHomePage";
-    }
     
     //Gain temporary access to create new password page
     @RequestMapping(value = {"/createNewPassword"},method = RequestMethod.GET)
