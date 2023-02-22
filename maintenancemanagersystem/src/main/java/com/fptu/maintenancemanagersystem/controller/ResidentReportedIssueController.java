553dba027b7bf0f18ce79b65a27e20eae4da60c8
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-02-22 16:12:53 +0700

    Update ResidentReportedIssueController.java

--- maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/controller/ResidentReportedIssueController.java
+++ maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/controller/ResidentReportedIssueController.java
@@ -1,7 +1,9 @@
 package com.fptu.maintenancemanagersystem.controller;
 
 import com.fptu.maintenancemanagersystem.model.ResidentReportedIssue;
+import com.fptu.maintenancemanagersystem.model.Room;
 import com.fptu.maintenancemanagersystem.service.ResidentReportedIssue.ResidentReportedIssueService;
+import com.fptu.maintenancemanagersystem.service.Room.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,14 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class ResidentReportedIssueController {
     @Autowired
     ResidentReportedIssueService residentReportedIssueService;
 
+    @Autowired
+    RoomService roomService;
+
     @GetMapping("/createNewReport")
     public String showCreateNewReportForm(Model model) {
         model.addAttribute("residentReportedIssue", new ResidentReportedIssue());
+        List<Room> allRooms = roomService.getAllRooms();
+        model.addAttribute("allRooms", allRooms);
         return "reportForm/residentReportFaultDevice";
     }
 
