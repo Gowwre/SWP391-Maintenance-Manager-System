ba0aa28275a2134d4986a6540da1f3952675f001
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-02-22 16:12:06 +0700

    Update ResidentReportedIssue.java

--- maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/model/ResidentReportedIssue.java
+++ maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/model/ResidentReportedIssue.java
@@ -7,69 +7,14 @@ import lombok.Setter;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-@NoArgsConstructor
+@NoArgsConstructor @Getter @Setter
 public class ResidentReportedIssue implements Serializable {
     private int issueId;
-    private int roomId;
+    private int roomNumber;
     private String residentName;
     private String residentEmail;
     private String residentPhoneNumber;
     private String description;
     private LocalDate dateReported;
 
-    public int getIssueId() {
-        return issueId;
-    }
-
-    public void setIssueId(int issueId) {
-        this.issueId = issueId;
-    }
-
-    public int getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
-    }
-
-    public String getResidentName() {
-        return residentName;
-    }
-
-    public void setResidentName(String residentName) {
-        this.residentName = residentName;
-    }
-
-    public String getResidentEmail() {
-        return residentEmail;
-    }
-
-    public void setResidentEmail(String residentEmail) {
-        this.residentEmail = residentEmail;
-    }
-
-    public String getResidentPhoneNumber() {
-        return residentPhoneNumber;
-    }
-
-    public void setResidentPhoneNumber(String residentPhoneNumber) {
-        this.residentPhoneNumber = residentPhoneNumber;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getDateReported() {
-        return LocalDate.now();
-    }
-
-    public void setDateReported(LocalDate dateReported) {
-        this.dateReported = dateReported;
-    }
 }
