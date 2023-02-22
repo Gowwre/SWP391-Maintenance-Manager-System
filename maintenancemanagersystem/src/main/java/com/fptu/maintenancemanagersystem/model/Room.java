4b5476772b476db594b6c6240ac9f9a719446f70
Gowwre <56794916+Gowwre@users.noreply.github.com>
2023-02-22 16:12:00 +0700

    Create Room.java

---
+++ maintenancemanagersystem/src/main/java/com/fptu/maintenancemanagersystem/model/Room.java
@@ -0,0 +1,16 @@
+package com.fptu.maintenancemanagersystem.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor @Getter @Setter
+public class Room implements Serializable {
+    private int roomId;
+    private String roomNumber;
+    private int floorId;
+    private boolean isOccupied;
+    private int staffId;
+}
